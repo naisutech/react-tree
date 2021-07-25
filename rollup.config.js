79a2c9aae28c50292d4c948d653536aea5b49ac7
ortonomy <git@ortonomy.co>
2021-07-25 23:34:26 +0800

    ensure type definitions are bundled with release

--- rollup.config.js
+++ rollup.config.js
@@ -21,10 +21,7 @@ export default [
       }),
       peerDepsExternal(),
       nodeResolve({ preferBuiltins: false }),
-      typescript({
-        sourceMap: true,
-        exclude: '**/__tests__/**, **/stories/**, **/mocks/**'
-      }),
+      typescript({ tsconfig: './tsconfig.build.json' }),
       commonjs(),
       graph(graphOptions),
       sizeSnapshot(),
